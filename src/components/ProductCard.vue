4a10afba8a5c12c14c57409caef1ff409063c136
Sirbuland Atlas Khan <sirbuland.atlas@gmail.com>
2023-11-28 22:35:34 +0500

    adds scroll menu and product card components

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <li class="product-item">
+    <article>
+      <h2>{{ category }} -- {{ item.name }}</h2>
+      <p>{{ item.details }}</p>
+      <br>
+
+      {{ item.price }}
+    </article>
+
+    <img src="https://picsum.photos/150/150">
+  </li>
+</template>
+
+<script type="text/javascript">
+  export default {
+    name: 'ProductCard',
+    props: {
+      category: {
+        type: String
+      },
+
+      item: {
+        required: true
+      }
+    }
+  };
+</script>
---
+++ src/components/ScrollMenu.vue
@@ -0,0 +1,54 @@
+<template>
+  <IndexBar :index-list="categories">
+    <section v-for="category in categories" :key="category">
+      <IndexAnchor :index="category">
+        {{ category }}
+      </IndexAnchor>
+
+      <ul
+        v-for="num in 4"
+        :key="`${category}-${num}`"
+      >
+        <ProductCard :category="category" :item="item" />
+      </ul>
+    </section>
+  </IndexBar>
+</template>
+
+<script>
+import { IndexBar, IndexAnchor } from 'vant'
+import ProductCard from './ProductCard'
+
+export default {
+  name: 'ScrollMenu',
+  components: {
+    IndexBar,
+    IndexAnchor,
+    ProductCard
+  },
+
+  data() {
+    return {
+      categories: [
+        'Populair ❤',
+        'Menus & Chicken Mix',
+        'The BOX',
+        'Voorgerechten',
+        'Pan pizza medium',
+        'Classic pizza medium',
+        'Pan pizza large',
+        'Classic pizza large',
+        'Cheesy crust pizza large',
+        'Desserten',
+        'Dranken'
+      ],
+
+      item: {
+        name: 'Pepperoni lovers pan pizza medium',
+        details: 'Pepperoniworst, mozzarella, tomatensaus.',
+        price: '$15.0'
+      }
+    }
+  }
+};
+</script>
